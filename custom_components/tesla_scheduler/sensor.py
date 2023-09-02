4981c5888276df44f57a92958eb6570daf794fcc
Victor Mylle <victor.mylle@hotmail.com>
2023-09-02 21:57:21 +0200

    info logging to warning

--- custom_components/tesla_scheduler/sensor.py
+++ custom_components/tesla_scheduler/sensor.py
@@ -5,14 +5,14 @@ import logging
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
-    _LOGGER.info("Setting up sensors...")
+    _LOGGER.warning("Setting up sensors...")
 
     ip_address = config_entry.data["ip_address"]
     endpoint = f"http://{ip_address}/your_endpoint"
 
     try:
         data = await hass.async_add_executor_job(lambda: requests.get(endpoint).json())
-        _LOGGER.info(f"Retrieved data: {data}")
+        _LOGGER.warning(f"Retrieved data: {data}")
     except Exception as e:
         _LOGGER.error(f"Could not retrieve data: {e}")
         return
@@ -21,7 +21,7 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     for key, value in data.items():
         sensors.append(MySensor(key, value))
 
-    _LOGGER.info(f"Adding {len(sensors)} sensors.")
+    _LOGGER.warning(f"Adding {len(sensors)} sensors.")
     async_add_entities(sensors, True)
 
 

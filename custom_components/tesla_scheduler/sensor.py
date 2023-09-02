46f89512775caf1eb7c7721581a8d213c7029c16
Victor Mylle <victor.mylle@hotmail.com>
2023-09-02 21:37:24 +0200

    Added more logging

--- custom_components/tesla_scheduler/sensor.py
+++ custom_components/tesla_scheduler/sensor.py
@@ -1,19 +1,29 @@
-from homeassistant.helpers.entity import Entity
 import requests
+from homeassistant.helpers.entity import Entity
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
+    _LOGGER.info("Setting up sensors...")
+
     ip_address = config_entry.data["ip_address"]
     endpoint = f"http://{ip_address}/your_endpoint"
 
-    # Fetch data asynchronously using the executor
-    data = await hass.async_add_executor_job(lambda: requests.get(endpoint).json())
+    try:
+        data = await hass.async_add_executor_job(lambda: requests.get(endpoint).json())
+        _LOGGER.info(f"Retrieved data: {data}")
+    except Exception as e:
+        _LOGGER.error(f"Could not retrieve data: {e}")
+        return
 
     sensors = []
     for key, value in data.items():
         sensors.append(MySensor(key, value))
-    # ... Repeat for other fields in your JSON
 
+    _LOGGER.info(f"Adding {len(sensors)} sensors.")
     async_add_entities(sensors, True)
+
 
 class MySensor(Entity):
     def __init__(self, name, state):

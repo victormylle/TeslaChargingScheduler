69c85f7dc3557771643a2b55be3c336a406fdf9b
Victor Mylle <victor.mylle@hotmail.com>
2023-09-03 00:05:43 +0200

    Fixed __init__ to add switch

--- custom_components/tesla_scheduler/__init__.py
+++ custom_components/tesla_scheduler/__init__.py
@@ -4,7 +4,11 @@ async def async_setup(hass, config):
     return True
 
 async def async_setup_entry(hass, config_entry):
+    # Forward the config entry to the sensor and switch platforms
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
+    )
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(config_entry, "switch")
     )
     return True

3d626716ba5c4a4c04c9e6ccd8f46623b27ffee5
Victor Mylle <victor.mylle@hotmail.com>
2023-09-02 21:25:57 +0200

    Fixing issues

--- custom_components/tesla_scheduler/sensor.py
+++ custom_components/tesla_scheduler/sensor.py
@@ -1,12 +1,12 @@
-import requests
 from homeassistant.helpers.entity import Entity
+import requests
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
     ip_address = config_entry.data["ip_address"]
     endpoint = f"http://{ip_address}/your_endpoint"
 
-    # Fetch data
-    data = requests.get(endpoint).json()
+    # Fetch data asynchronously using the executor
+    data = await hass.async_add_executor_job(lambda: requests.get(endpoint).json())
 
     sensors = []
     for key, value in data.items():

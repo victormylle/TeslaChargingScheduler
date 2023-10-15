265939a13b59fc55010078c7b5ee7e5151ca4d83
Victor Mylle <victor.mylle@hotmail.com>
2023-10-15 13:03:12 +0200

    Added charging cost sensors

--- custom_components/tesla_scheduler/sensor.py
+++ custom_components/tesla_scheduler/sensor.py
@@ -23,6 +23,12 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     for key, value in data.items():
         sensors.append(MySensor(key, value, hass, endpoint))
 
+    periods = ["today", "yesterday", "month", "year"]
+    for period in periods:
+        sensor_name = f"charging_cost_{period}"
+        sensors.append(ChargingCostSensor(sensor_name, hass, ip_address, period))
+
+
     _LOGGER.warning(f"Adding {len(sensors)} sensors.")
     async_add_entities(sensors, True)
 
@@ -55,3 +61,52 @@ class MySensor(Entity):
             _LOGGER.warning(f"Updated sensor {self._name} to {self._state}")
         except Exception as e:
             _LOGGER.error(f"Could not update sensor {self._name}: {e}")
+
+class ChargingCostSensor(Entity):
+    def __init__(self, name, hass, ip_address, period):
+        self._name = name
+        self._state = 0.0  # Default state
+        self._hass = hass
+        self._period = period
+        self._endpoint = f"http://{ip_address}/charging_cost?date={period}"
+
+        # Update every minute
+        async_track_time_interval(self._hass, self.async_update, datetime.timedelta(minutes=1))
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def unique_id(self):
+        return self._name
+
+    async def async_update(self, *args):
+        try:
+            start_date, end_date = self._get_date_range()
+            endpoint = f"{self._base_endpoint}?start_date={start_date}&end_date={end_date}"
+            response = await self._hass.async_add_executor_job(lambda: requests.get(endpoint).json())
+            self._state = response.get("total_cost", 0.0)
+            _LOGGER.warning(f"Updated sensor {self._name} to {self._state}")
+        except Exception as e:
+            _LOGGER.error(f"Could not update sensor {self._name}: {e}")
+
+    def _get_date_range(self):
+        today = datetime.date.today()
+        if self._period == "today":
+            return today, today
+        elif self._period == "yesterday"":
+            yesterday = today - datetime.timedelta(days=1)
+            return yesterday, yesterday
+        elif self._period == "month"":
+            start_date = today.replace(day=1)
+            end_date = today
+            return start_date, end_date
+        elif self._period == "year":
+            start_date = today.replace(month=1, day=1)
+            end_date = today
+            return start_date, end_date
